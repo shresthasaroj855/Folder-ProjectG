bec3e4f2bcc1eb917c881620f5f5f2e4788a7c63
Saroj shrestha <shresthasaroj855@gmail.com>
2024-01-23 21:10:38 +0545

    first hammer

--- Project/obj/Debug/net7.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net7.0/Project.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee622df26d9ef82723d12ed0141c2f614642079d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
